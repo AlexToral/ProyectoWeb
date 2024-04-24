5f9e139294848ecb4147b374db095acca9040322
Alex Toral <alesu004@gmail.com>
2024-04-24 17:08:56 -0600

    Se añadio Personal Data y Sidebar

---
+++ app/javascript/sidebar.js
@@ -0,0 +1,16 @@
+"use strict";
+let sb = document.getElementById("right_sidebar");
+function Sidebarcheck(){
+    
+    console.log("HOLA");
+
+    if(sb.style.display != "none"){
+        SidebarHide()
+    }else{
+        sb.style.display = "block";
+    }
+}
+
+function SidebarHide(){
+    sb.style.display = "none";
+}
