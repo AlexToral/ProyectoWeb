d77bbcd4fc9bb43315d12aea412550cdba23b89c
Alex Toral <alesu004@gmail.com>
2024-05-04 11:31:47 -0600

    Se añadio la caja de comentarios

--- app/javascript/utils.js
+++ app/javascript/utils.js
@@ -1,5 +1,6 @@
 "use strict";
 
+//Sidebar 
 let sb = document.getElementById("right_sidebar");
 
 function Sidebarcheck(){
@@ -14,3 +15,20 @@ function Sidebarcheck(){
 function SidebarHide(){
     sb.style.display = "none";
 }
+//End of Sidebar
+
+//Inside_blog Buttons
+let like_button = document.getElementById("btn-like");
+
+let isLiked = false;
+
+function stateChangeLikes(){
+   if (isLiked) {
+      like_button.innerHTML = "<i class='far fa-heart'></i>";
+      isLiked = false;
+   } else {
+      like_button.innerHTML = "<i class='fa fa-heart' aria-hidden='true'></i>";
+      isLiked = true;
+   }
+}
+//Inside_blog Buttons end
