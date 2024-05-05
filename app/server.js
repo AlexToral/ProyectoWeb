9a9a62a82a1ef1b75dbabfe059f9f6a5fbce451c
Glueman93 <marcospc9324@gmail.com>
2024-05-04 19:19:42 -0600

    ya funciona la base de dato

--- app/server.js
+++ app/server.js
@@ -3,14 +3,26 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const mongoose = require('mongoose'); // Importar mongoose
+
 const app = express();
 const port = 3000;
 const router = require('././javascript/router');
 
+mongoose.connect('mongodb+srv://Admin:bato1993@cluster0.0linyln.mongodb.net/', { // Conectar a MongoDB
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Error de conexión:')); // Manejo de errores
+db.once('open', () => {
+    console.log('Conectado a MongoDB'); // Conexión exitosa
+});
+
 app.use(cors());
 app.use(router);
 app.use(express.json());
-
 
 app.use(express.static(path.join(__dirname, 'view')));
 app.use(express.static(path.join(__dirname, 'javascript')));
@@ -20,15 +32,16 @@ app.get('/javascript/utils.js', (req, res) => {
     res.sendFile(path.join(__dirname, 'javascript', 'utils.js'));
 });
 
-app.get(['/','/home'],(req,res)=>
+app.get(['/','/home'],(req,res) =>
 {
-    res.sendFile(path.join(__dirname,'view','home.html'));
+    res.sendFile(path.join(__dirname, 'view', 'home.html'));
 });
 
-app.get('/profile',(req,res)=>
+app.get('/profile', (req, res) =>
 {
-    res.sendFile(path.join(__dirname,'view','personal_data.html'));
+    res.sendFile(path.join(__dirname, 'view', 'personal_data.html'));
 });
+
 app.listen(port, () => {
-    console.log(`Server corriendo en el puerto ${port}`);
-}); 
+    console.log(`Servidor corriendo en el puerto ${port}`);
+});
